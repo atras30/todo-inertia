1c6ec42f2a83509a65d5b8fd7c3214b38828c99f
Atras Shalhan <90594355+atras30@users.noreply.github.com>
2023-07-04 16:19:57 +0700

    init Migration

---
+++ database/migrations/2023_07_04_132237_create_notes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->id();
+            $table->text("title");
+            $table->text("body");
+            $table->boolean("is_done")->default(false);
+            $table->enum('visibility', ['public', 'private', 'unlisted']);
+            $table->foreignId('user_id')
+                ->nullable()
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notes');
+    }
+};
