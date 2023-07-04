b67935bd3728f311cdd1c8473dc8645ff945c4b5
Atras Shalhan <90594355+atras30@users.noreply.github.com>
2023-07-04 16:19:05 +0700

    create routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\NoteController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -25,9 +27,11 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/notes', [NoteController::class, 'index'])->name('notes');
+Route::get('/notes/paginate', [NoteController::class, 'paginateNotes'])->name('notes.paginate');
+Route::get('/notes/create', [NoteController::class, 'showCreatNoteForm'])->name('notes.form');
+Route::post('/notes/create', [NoteController::class, 'create'])->name('notes.create');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
